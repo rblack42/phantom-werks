0a98f03b234da1865ec5a1590f71c29844dc1fa0
Roie Black <roie.black@gmail.com>
2020-09-28 11:40:33 -0500

    whitenoise is serving docs

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
+import os
 from flask import Flask
-app = Flask(__name__)
+from whitenoise import WhiteNoise
 
-@app.route("/")
+
+app = Flask(__name__)
+app.wsgi_app = WhiteNoise(app.wsgi_app,
+        root=os.path.join(os.path.dirname(__file__), 'docs'),
+        prefix='')
+
+@app.route("/hello")
 def hello():
     return "Hello World!"
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
